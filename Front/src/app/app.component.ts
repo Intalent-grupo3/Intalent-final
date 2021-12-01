1e1ac37f723781b4b5de30b981180a9260a78c37
callmeferre <callmeferre@gmail.com>
2021-12-01 12:01:00 +0100

    cambiado de lugar a los componentes para que mantengan el routing

--- Front/src/app/app.component.ts
+++ Front/src/app/app.component.ts
@@ -1,22 +1,10 @@
 import { Component } from '@angular/core';
-import { Subject} from 'rxjs';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'lucatic-grupo3-final';
-  parentSubject:Subject<string> = new Subject();
-
-  constructor() {
-
-  }
-
- cardAnimation(value: any) {
-    this.parentSubject.next(value);
-  }
-
+    constructor() {}
 }
-
