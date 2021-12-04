14c45fc7e55e52c9b08a5d6dd46b2a63306497c3
callmeferre <callmeferre@gmail.com>
2021-12-04 01:24:02 +0100

    account component

---
+++ Front/src/app/components/account/profile/profile.component.ts
@@ -0,0 +1,43 @@
+import { Component, NgZone, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { Persona } from 'src/app/models/user';
+import { CrudServicesService } from 'src/app/services/crud-services.service';
+
+@Component({
+    selector: 'app-profile',
+    templateUrl: './profile.component.html',
+    styleUrls: ['./profile.component.scss'],
+})
+export class ProfileComponent implements OnInit {
+    loginId: any;
+    persona: Persona = {} as Persona;
+    constructor(
+        // public User: CreateProfileComponent,
+        private crudService: CrudServicesService,
+        private router: Router,
+        private ngZone: NgZone
+    ) {
+        this.loginId; //aquí aparece el id que hemos guardado del login de yelder.
+    }
+
+    ngOnInit(): any {
+        this.crudService.showuserprofile(this.loginId).subscribe((res) => {
+            console.log(res);
+            this.persona = res;
+        });
+    }
+    delete() {
+        console.log(this.loginId);
+        if (window.confirm('Do you want to go ahead?')) {
+            this.crudService.deleteUserProfile(this.loginId).subscribe({
+                next: (any) => {
+                    console.log('Perfil borrado correctamente');
+                    this.ngZone.run(() => this.router.navigateByUrl('/login'));
+                },
+                error: (err) => {
+                    console.log(err);
+                },
+            });
+        }
+    }
+}
